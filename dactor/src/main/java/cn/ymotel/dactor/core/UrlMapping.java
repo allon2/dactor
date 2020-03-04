b0de762a86b49379ae29bfdf55a6d5cf54025fba
allon2 <allon2@163.com>
2020-03-04 15:09:59 +0800

    去掉默认静态页面目录

--- dactor/src/main/java/cn/ymotel/dactor/core/UrlMapping.java
+++ dactor/src/main/java/cn/ymotel/dactor/core/UrlMapping.java
@@ -6,11 +6,11 @@ import java.util.Map;
 
 public class UrlMapping {
     private static java.util.Map<String,Object> Urlmapping =new java.util.concurrent.ConcurrentHashMap<>();
-    static{
-        Urlmapping.put("/static/**",new StaticResourceRequestHandler());
-        ///**
-//        mapping.put("/**",new StaticResourceRequestHandler());
-    }
+//    static{
+////        Urlmapping.put("/static/**",new StaticResourceRequestHandler());
+//        ///**
+////        mapping.put("/**",new StaticResourceRequestHandler());
+//    }
 
     public static void addMapping(String urlpattern ,ActorTransactionCfg cfg){
         if(urlpattern==null||urlpattern.trim().equals("")){
